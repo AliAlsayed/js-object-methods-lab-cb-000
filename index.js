2e6400e539398f9a99e54600d86587ea52308c63
Ali Alsayed <a_alsayed87@hotmail.com>
2019-07-03 09:14:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code solution here
+
+class President(name, politicalParty, yearsInOffice, homeState){
+  this.name = name;
+  this.politicalParty = politicalParty;
+  this.yearsInOffice = yearsInOffice;
+  this.homeState = homeState;
+}
